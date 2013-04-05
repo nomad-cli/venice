410109cfa5390752fd8678ecd0a950f29336c65f
Ben Scheirman <ben@scheirman.com>
2013-04-05 08:47:46 -0500

    Allow Venice to contain a shared secret

--- lib/venice.rb
+++ lib/venice.rb
@@ -1,5 +1,14 @@
 module Venice
   VERSION = "0.0.1"
+
+  def self.shared_secret
+    @@shared_secret
+  end
+
+  def self.shared_secret=(secret)
+    @@shared_secret = secret
+  end
+  self.shared_secret = nil
 end
 
 require 'venice/client'
