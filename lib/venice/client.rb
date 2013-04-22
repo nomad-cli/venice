bcbc70c706c90450b04237f7b503a481816c99dc
Mattt Thompson <m@mattt.me>
2013-04-22 14:47:17 -0700

    Refactoring verification logic into client

--- lib/venice/client.rb
+++ lib/venice/client.rb
@@ -6,8 +6,46 @@ module Venice
   ITUNES_PRODUCTION_RECEIPT_VERIFICATION_ENDPOINT = "https://buy.itunes.apple.com/verifyReceipt"
   ITUNES_DEVELOPMENT_RECEIPT_VERIFICATION_ENDPOINT = "https://sandbox.itunes.apple.com/verifyReceipt"
 
+  RECEIPT_VERIFICATION_ERRORS_BY_STATUS_CODE = {
+    21000 => InvalidJSONObjectError,
+    21002 => MalformedReceiptDataError,
+    21003 => ReceiptVerificationAuthenticationError,
+    21004 => SharedSecretMismatchError,
+    21005 => ReceiptVerificationServerOfflineError,
+    21006 => ValidReceiptExpiredSubscriptionError,
+    21007 => SandboxReceiptSentToProductionError,
+    21008 => ProductionReceiptSentToSandboxError
+  }
+
+  class ReceiptVerificationError < StandardError; end
+
+  # 21000: The App Store could not read the JSON object you provided.
+  class InvalidJSONObjectError < ReceiptVerificationError; end
+
+  # 21002: The data in the receipt-data property was malformed.
+  class MalformedReceiptDataError < ReceiptVerificationError; end
+
+  # 21003: The receipt could not be authenticated.
+  class ReceiptVerificationAuthenticationError < ReceiptVerificationError; end
+
+  # 21004: The shared secret you provided does not match the shared secret on file for your account.
+  class SharedSecretMismatchError < ReceiptVerificationError; end
+
+  # 21005: The receipt server is not currently available.
+  class ReceiptVerificationServerOfflineError < ReceiptVerificationError; end
+
+  # 21006: This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data is also decoded and returned as part of the response.
+  class ValidReceiptExpiredSubscriptionError < ReceiptVerificationError; end
+
+  # 21007: This receipt is a sandbox receipt, but it was sent to the production service for verification.
+  class SandboxReceiptSentToProductionError < ReceiptVerificationError; end
+
+  # 21008: This receipt is a production receipt, but it was sent to the sandbox service for verification.
+  class ProductionReceiptSentToSandboxError < ReceiptVerificationError; end
+
   class Client
     attr_accessor :verification_url
+    attr_writer :shared_secret
 
     class << self
       def development
@@ -21,22 +59,42 @@ module Venice
         client.verification_url = ITUNES_PRODUCTION_RECEIPT_VERIFICATION_ENDPOINT
         client
       end
-
-      def shared_secret=(secret)
-        @@shared_secret = secret
-      end
     end
 
     def initialize
       @verification_url = ENV['IAP_VERIFICATION_ENDPOINT']
     end
 
-    def verify!(data)
+    def verify!(data, options = {})
+      json = json_response_from_verifying_data(data)
+      status, receipt_attributes = json['status'].to_i, json['receipt']
+
+      case status
+      when 0, 21006
+        receipt = Receipt.new(receipt_attributes)
+
+        if latest_receipt_attributes = json['latest_receipt_info']
+          receipt.latest = Receipt.new(latest_receipt_attributes)
+        end
+
+        if latest_expired_receipt_attributes = json['latest_expired_receipt_info']
+          receipt.latest_expired = Receipt.new(latest_expired_receipt_attributes)
+        end
+
+        return receipt
+      else
+        raise (RECEIPT_VERIFICATION_ERRORS_BY_STATUS_CODE[status] || ReceiptVerificationError), "#{status}"
+      end
+    end
+
+    private
+
+    def json_response_from_verifying_data(data)
       parameters = {
         'receipt-data' => data
       }
 
-      parameters['password'] = @@shared_secret rescue nil
+      parameters['password'] = @shared_secret rescue nil
 
       uri = URI(@verification_url)
       http = Net::HTTP.new(uri.host, uri.port)
@@ -47,6 +105,7 @@ module Venice
       request.body = parameters.to_json
 
       response = http.request(request)
+
       JSON.parse(response.body)
     end
   end
--- lib/venice/receipt.rb
+++ lib/venice/receipt.rb
@@ -1,43 +1,6 @@
 require 'time'
 
 module Venice
-  class ReceiptVerificationError < StandardError; end
-
-  # 21000: The App Store could not read the JSON object you provided.
-  class InvalidJSONObjectError < ReceiptVerificationError; end
-
-  # 21002: The data in the receipt-data property was malformed.
-  class MalformedReceiptDataError < ReceiptVerificationError; end
-
-  # 21003: The receipt could not be authenticated.
-  class ReceiptVerificationAuthenticationError < ReceiptVerificationError; end
-
-  # 21004: The shared secret you provided does not match the shared secret on file for your account.
-  class SharedSecretMismatchError < ReceiptVerificationError; end
-
-  # 21005: The receipt server is not currently available.
-  class ReceiptVerificationServerOfflineError < ReceiptVerificationError; end
-
-  # 21006: This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data is also decoded and returned as part of the response.
-  class ValidReceiptExpiredSubscriptionError < ReceiptVerificationError; end
-
-  # 21007: This receipt is a sandbox receipt, but it was sent to the production service for verification.
-  class SandboxReceiptSentToProductionError < ReceiptVerificationError; end
-
-  # 21008: This receipt is a production receipt, but it was sent to the sandbox service for verification.
-  class ProductionReceiptSentToSandboxError < ReceiptVerificationError; end
-
-  RECEIPT_VERIFICATION_ERRORS_BY_STATUS_CODE = {
-    21000 => InvalidJSONObjectError,
-    21002 => MalformedReceiptDataError,
-    21003 => ReceiptVerificationAuthenticationError,
-    21004 => SharedSecretMismatchError,
-    21005 => ReceiptVerificationServerOfflineError,
-    21006 => ValidReceiptExpiredSubscriptionError,
-    21007 => SandboxReceiptSentToProductionError,
-    21008 => ProductionReceiptSentToSandboxError
-  }
-
   class Receipt
     # The number of items purchased. This value corresponds to the quantity property of the SKPayment object stored in the transaction’s payment property.
     attr_reader :quantity
@@ -126,26 +89,7 @@ module Venice
         client = Client.production
 
         begin
-          json = client.verify!(data)
-          status, receipt_attributes = json['status'].to_i, json['receipt']
-
-          case status
-          when 0, 21006
-            receipt = Receipt.new(receipt_attributes)
-
-            if latest_receipt_attributes = json['latest_receipt_info']
-              receipt.latest = Receipt.new(latest_receipt_attributes)
-            end
-
-            if latest_expired_receipt_attributes = json['latest_expired_receipt_info']
-              receipt.latest_expired = Receipt.new(latest_expired_receipt_attributes)
-            end
-
-            return receipt
-          else
-            raise (RECEIPT_VERIFICATION_ERRORS_BY_STATUS_CODE[status] || ReceiptVerificationError), "#{status}"
-          end
-
+          client.verify!(data, options)
         rescue => error
           case error
           when SandboxReceiptSentToProductionError
