4155dd75af7f59178f03fb89c4cfec9cc34c5e10
Mattt Thompson <m@mattt.me>
2013-04-22 19:20:37 -0700

    Raising error messages directly

--- lib/venice/client.rb
+++ lib/venice/client.rb
@@ -6,41 +6,15 @@ module Venice
   ITUNES_PRODUCTION_RECEIPT_VERIFICATION_ENDPOINT = "https://buy.itunes.apple.com/verifyReceipt"
   ITUNES_DEVELOPMENT_RECEIPT_VERIFICATION_ENDPOINT = "https://sandbox.itunes.apple.com/verifyReceipt"
 
-  class ReceiptVerificationError < StandardError; end
-
-  # 21000: The App Store could not read the JSON object you provided.
-  class InvalidJSONObjectError < ReceiptVerificationError; end
-
-  # 21002: The data in the receipt-data property was malformed.
-  class MalformedReceiptDataError < ReceiptVerificationError; end
-
-  # 21003: The receipt could not be authenticated.
-  class ReceiptVerificationAuthenticationError < ReceiptVerificationError; end
-
-  # 21004: The shared secret you provided does not match the shared secret on file for your account.
-  class SharedSecretMismatchError < ReceiptVerificationError; end
-
-  # 21005: The receipt server is not currently available.
-  class ReceiptVerificationServerOfflineError < ReceiptVerificationError; end
-
-  # 21006: This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data is also decoded and returned as part of the response.
-  class ValidReceiptExpiredSubscriptionError < ReceiptVerificationError; end
-
-  # 21007: This receipt is a sandbox receipt, but it was sent to the production service for verification.
-  class SandboxReceiptSentToProductionError < ReceiptVerificationError; end
-
-  # 21008: This receipt is a production receipt, but it was sent to the sandbox service for verification.
-  class ProductionReceiptSentToSandboxError < ReceiptVerificationError; end
-
   RECEIPT_VERIFICATION_ERRORS_BY_STATUS_CODE = {
-    21000 => InvalidJSONObjectError,
-    21002 => MalformedReceiptDataError,
-    21003 => ReceiptVerificationAuthenticationError,
-    21004 => SharedSecretMismatchError,
-    21005 => ReceiptVerificationServerOfflineError,
-    21006 => ValidReceiptExpiredSubscriptionError,
-    21007 => SandboxReceiptSentToProductionError,
-    21008 => ProductionReceiptSentToSandboxError
+    21000 => "The App Store could not read the JSON object you provided.",
+    21002 => "The data in the receipt-data property was malformed.",
+    21003 => "The receipt could not be authenticated.",
+    21004 => "The shared secret you provided does not match the shared secret on file for your account.",
+    21005 => "The receipt server is not currently available.",
+    21006 => "This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data is also decoded and returned as part of the response.",
+    21007 => "This receipt is a sandbox receipt, but it was sent to the production service for verification.",
+    21008 => "This receipt is a production receipt, but it was sent to the sandbox service for verification."
   }
 
   class Client
@@ -83,7 +57,7 @@ module Venice
 
         return receipt
       else
-        raise (RECEIPT_VERIFICATION_ERRORS_BY_STATUS_CODE[status] || ReceiptVerificationError), "#{status}"
+        raise (RECEIPT_VERIFICATION_ERRORS_BY_STATUS_CODE[status] || "Unknown Error: #{status}")
       end
     end
 
