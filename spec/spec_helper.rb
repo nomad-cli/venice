4bb2298f3c83c45dd05f00f8e25463010d5e32e7
Amos Lanka <amoslanka@gmail.com>
2014-06-05 23:48:50 -0700

    Spec helper and config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ end
 require 'venice'
 require 'rspec'
 
-Rspec.configure do |config|
+RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.yield_receiver_to_any_instance_implementation_blocks = false
