11c8beaa7ceef6596e01cf4ab87b9a068499bb07
Riho Takagi <riho.takagi@vasily.jp>
2018-07-31 01:10:24 +0900

    Add is_trial_period field (#64)
    * Add is_trial_period
    
    * translate string 'true/false' to boolean
    
    * add spec

--- lib/venice/in_app_receipt.rb
+++ lib/venice/in_app_receipt.rb
@@ -54,6 +54,7 @@ module Venice
       @purchased_at = DateTime.parse(attributes['purchase_date']) if attributes['purchase_date']
       @app_item_id = attributes['app_item_id']
       @version_external_identifier = attributes['version_external_identifier']
+      @is_trial_period = to_bool(attributes['is_trial_period'])
 
       # expires_date is in ms since the Epoch, Time.at expects seconds
       if attributes['expires_date_ms']
@@ -86,6 +87,7 @@ module Venice
         original_purchase_date: (@original.purchased_at.httpdate rescue nil),
         app_item_id: @app_item_id,
         version_external_identifier: @version_external_identifier,
+        is_trial_period: @is_trial_period,
         expires_at: (@expires_at.httpdate rescue nil),
         cancellation_at: (@cancellation_at.httpdate rescue nil)
       }
@@ -100,6 +102,11 @@ module Venice
     
     def is_number?(string)
       !!(string && string.to_s =~ /^[0-9]+$/)
+    end
+
+    def to_bool(string)
+      return string if string.is_a?(TrueClass) || string.is_a?(FalseClass)
+      {'true' => true, 'false' => false }[string]
     end
   end
 end
